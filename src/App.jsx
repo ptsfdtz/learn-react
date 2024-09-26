94fb04a6a42e68bdcd4f376b94cbb1f5b91f3398
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-09-27 00:40:25 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css';
 import React from 'react';
 
-class App extends React.Component {
-  button=(num1, num2, num3) => {
-    console.log(num1, num2, num3);
+function App() {
+  function HelloWorld() {
+    return <div>Hello World</div>;
   }
-    
-  render() {
+  class ClassHello extends React.Component {
+    render() {
+      return <div>Hello World</div>;
+    }
+  }
+  let hello = HelloWorld();
+
   return (
     <div className="App">
-       {/* <div onClick={this.button}>button</div> */}
-       {/* <div></div> */}
-       <div onClick={this.button.bind(this, 1, 2, 3)}>button</div>
+      {hello}
+      <HelloWorld />
+      <ClassHello />
     </div>
-    );
-  }
-} 
+  );
+}
+
 
 export default App;
